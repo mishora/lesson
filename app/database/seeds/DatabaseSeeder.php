833c662e5b1d493d866e45901d6ef034eeac8a37
Mihail Kovachev <mishoria@gmail.com>
2014-10-06 08:14:45 +0000

    Dummy Users and Statuses

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -3,6 +3,22 @@
 class DatabaseSeeder extends Seeder {
 
 	/**
+	 * @var array
+	 */
+	protected $tables =[
+		'users',
+		'statuses'
+	];
+
+	/**
+	 * @var array
+	 */
+	protected $seeders = [
+		'UsersTableSeeder',
+		'StatusesTableSeeder'
+	];
+
+	/**
 	 * Run the database seeds.
 	 *
 	 * @return void
@@ -11,7 +27,32 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->cleanDatabase();
+		$this->seedDatabase();
+	}
+
+	/**
+	 * Clean out the database for a new seed generation
+	 */
+	private function cleanDatabase()
+	{
+		DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+		foreach ($this->tables as $table) {
+			DB::table($table)->truncate();
+		}
+
+		DB::statement('SET FOREIGN_KEY_CHECKS=1');
+	}
+
+	/**
+	 * Sedd database
+	 */
+	private function seedDatabase()
+	{
+		foreach ($this->seeders as $seedClass) {
+			$this->call($seedClass);
+		}
 	}
 
 }
---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Faker\Factory as Faker;
+use Larabook\Users\User;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+
+		// Create default User
+		User::create([
+				'username' => 'mishora',
+				'email' => 'mishoria@gmail.com',
+				'password' => '123456'
+		]);
+
+		// Create Random Users
+		foreach(range(1, 50) as $index)
+		{
+			User::create([
+				'username' => $faker->word . $index,
+				'email' => $faker->email,
+				'password' => 'secret'
+			]);
+		}
+	}
+
+}
+

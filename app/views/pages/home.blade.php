42ec994dbbe7f6e122f164222eb27a1d7a2cab4a
Mihail Kovachev <mishoria@gmail.com>
2014-10-22 09:58:00 +0000

    Designing the Home Page

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.default')
 
 @section('content')
-	Welcome to GETIX
+	<div class="jumbotron">
+		<h1>Welcome to GETIX</h1>
+		<p>Welcome to the premier place to talk about Laravel with others. Why you not sign up to see what all the fuss is about?</p>
+		<p>
+			<a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign Up!</a>
+		</p>
+	</div>
 @stop

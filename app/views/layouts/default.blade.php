8a5aff428d6577ea07012ee6fe45b27a5a2f575f
Mihail Kovachev <mishoria@gmail.com>
2014-10-22 09:12:15 +0000

    The Master Page

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset=UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		@yield('content')
+	</div>
+
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
